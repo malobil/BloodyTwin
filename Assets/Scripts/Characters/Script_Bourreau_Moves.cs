bdd5357ea95cd5c3c97a28c0985e00deeb7d9296
Kevin Ibanez <ibanez13keke@gmail.com>
2018-02-28 18:38:40 +0100

    WIP bourreau mouvement
    Mouvement du bourreau par rapport à la camera (offline).

---
+++ Assets/Scripts/Characters/Script_Bourreau_Moves.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Bourreau_Moves : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public Transform cameraTransform;
+
+    private CharacterController cC;
+	// Use this for initialization
+	void Start ()
+    {
+        cC = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float movementXaxis = Input.GetAxis("Horizontal");
+        float movementYaxis = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(movementXaxis, 0, movementYaxis);
+
+        moveDirection *= moveSpeed;
+
+        cC.Move(moveDirection * Time.deltaTime);
+
+    }
+}
