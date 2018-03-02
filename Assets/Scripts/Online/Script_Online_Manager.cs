1cec9917bdd8a36e11e10a460ce72f8aeb6302da
OTAKU <otaku.suprem@gmail.com>
2018-03-02 12:48:54 +0100

    online manager script créé

---
+++ Assets/Scripts/Online/Script_Online_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Script_Online_Manager : NetworkLobbyManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
