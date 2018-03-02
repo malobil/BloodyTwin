f49a9279bde777be69f11eea1ddc19a02590a9a1
Michel <canet.ragot@gmail.com>
2018-03-02 11:28:41 +0100

    Charactert Standard Assets

---
+++ Assets/Scripts/Characters/Spectre/Script_Spectre_Moves.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Spectre_Moves : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Mouvement();
+	}
+
+    private void Mouvement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); // touche Q & D
+        float forwardInput = Input.GetAxis("Vertical"); // touche Z & S
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput); // déplacement Q & D
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); // déplacement Z & S
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+}
