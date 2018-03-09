959fbd02d2303dc18253d05e02cabe2382e49391
OTAKU <otaku.suprem@gmail.com>
2018-03-09 11:48:09 +0100

    Debut du niveau de peur globale

---
+++ Assets/Scripts/Level Of Fear/Script_Global_Fear.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_Global_Fear : MonoBehaviour {
+
+    private static Script_Global_Fear instance;
+    public static Script_Global_Fear Instance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void UpFear(float fearToAdd)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Level Of Fear/Script_Intruder_Fear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Intruder_Fear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Fear();
+	}
+
+    public void Fear()
+    {
+       // Script_Global_Fear.Instance.UpFear(10f);
+    }
+}
