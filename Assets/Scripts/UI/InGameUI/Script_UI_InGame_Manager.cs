d3c94eb8306ccd35344b95f956cbae0e4a3b9cae
Kevin Ibanez <ibanez13keke@gmail.com>
2018-03-19 10:35:39 +0100

    WIP Timer et condition de défaite

---
+++ Assets/Scripts/UI/InGameUI/Script_UI_InGame_Manager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class Script_UI_InGame_Manager : NetworkBehaviour {
+
+    [SyncVar]
+    public float minutes;
+    [SyncVar]
+    public float seconds;
+
+    public Text timerText;
+    public GameObject gameOverPanel;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!isServer)
+        {
+            return;
+        }
+
+        Timer();
+	}
+
+    void Timer()
+    {
+        if(seconds > 0)
+        {
+            seconds -= Time.deltaTime;
+            RpcUpdateTimerText();
+        }
+        else if(seconds <= 0 && minutes > 0)
+        {
+            seconds = 60;
+            minutes--;
+        }
+
+        if(seconds <= 0 && minutes <= 0)
+        {
+            RpcGameOver();
+        }
+    }
+
+    [ClientRpc]
+    void RpcUpdateTimerText()
+    {
+        timerText.text = minutes.ToString("") + " : " + Mathf.RoundToInt(seconds).ToString("");
+    }
+
+    [ClientRpc]
+    void RpcGameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    
+    public void DisconnectPlayer()
+    {
+        Debug.Log("Disconnect");
+        NetworkManager.singleton.StopHost();
+        //SceneManager.LoadScene(sceneToLoad);
+    }
+}
