d609e9650cfff7a87adab6f46568c328a1270d52
OTAKU <otaku.suprem@gmail.com>
2018-03-19 14:14:24 +0100

    AI waypoint

---
+++ Assets/Scripts/Intrus/Script_Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float debugDrawnRadius;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawnRadius);
+    }
+
+	
+}
