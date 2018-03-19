4bc79e3afe8c8dd2a2102098fc61d5d40fe2605c
Kevin Ibanez <ibanez13keke@gmail.com>
2018-03-19 12:40:47 +0100

    WIP intrus online

--- Assets/Scripts/Intrus/Script_Global_Fear_Online.cs
+++ Assets/Scripts/Intrus/Script_Global_Fear_Online.cs
@@ -1,16 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Script_Global_Fear_Online : MonoBehaviour {
+public class Script_Global_Fear_Online : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Image imgFearState;
+
+    [SyncVar]
+    private int intruderNumberTot;
+
+    public List<Script_Intruder> intruders = new List<Script_Intruder>();
+
+    [SyncVar]
+    private float currentFearState;
+
+    public static Script_Global_Fear_Online Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        //FearGlobalState();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log(intruderNumberTot);
+        }
+    }
+
+    public void IntruderAmount()
+    {
+       intruderNumberTot++;
+       Debug.Log(intruderNumberTot);
+    }
+
+    public void FearGlobalState()
+    {
+        currentFearState = 0;
+        foreach (Script_Intruder intruderFear in intruders)
+        {
+            currentFearState += intruderFear.CurrentFearState();
+        }
+
+        currentFearState /= intruderNumberTot;
+        Debug.Log("moyenne:" + currentFearState);
+        FearGraphics();
+    }
+
+    public void FearGraphics()
+    {
+        if(imgFearState !=null)
+        {
+            imgFearState.fillAmount = currentFearState / 100;
+        }
+        
+    }
 }
--- Assets/Scripts/Intrus/Script_Intruder_Online.cs
+++ Assets/Scripts/Intrus/Script_Intruder_Online.cs
@@ -1,16 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking ;
 
-public class Script_Intruder_Online : MonoBehaviour {
+public class Script_Intruder_Online : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float fearAdd;
+    public float fearRemoved;
+
+    public int intruderNum;
+    public Image fearLevel;
+    public float currentFear;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(isServer)
+        {
+            Script_Global_Fear_Online.Instance.IntruderAmount();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            FearedImpact(fearAdd);
+        }
+
+        if (Input.GetKeyDown("g"))
+        {
+            FearedImpact(fearRemoved);
+        }
+
+    }
+
+    public void IntruderDeath()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void FearedImpact(float fearState)
+    {
+
+        currentFear += fearState;
+        if (currentFear >= 100)
+        {
+            currentFear = 100;
+        }
+        else if (currentFear <= 0)
+        {
+            currentFear = 0;
+        }
+        Script_Global_Fear_Online.Instance.FearGlobalState();
+        UpdateFearFeedback();
+    }
+
+    public float CurrentFearState()
+    {
+        return currentFear;
+    }
+
+    private void UpdateFearFeedback()
+    {
+        fearLevel.fillAmount = currentFear / 100;
+    }
 }
