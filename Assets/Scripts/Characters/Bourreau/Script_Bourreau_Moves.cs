eaf93d473de9be9ecdc5c29ecf8574a5de6986ee
Kevin Ibanez <ibanez13keke@gmail.com>
2018-04-27 12:08:30 +0200

    *.*

--- Assets/Scripts/Characters/Bourreau/Script_Bourreau_Moves.cs
+++ Assets/Scripts/Characters/Bourreau/Script_Bourreau_Moves.cs
@@ -97,36 +97,40 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
             if (Input.GetButtonDown("Communication_Come"))
             {
-                CmdCommunicationCome();
+                
                 if(!comAudioSource.isPlaying)
                 {
                     CmdComSound(0);
+                    CmdCommunicationCome();
                 }
                 
             }
             else if (Input.GetButtonDown("Communication_GotOne"))
             {
-                CmdCommunicationGotOne();
+                
                 if (!comAudioSource.isPlaying)
                 {
                     CmdComSound(1);
+                    CmdCommunicationGotOne();
                 }
             }
             else if (Input.GetButtonDown("Communication_HeRunAway"))
             {
-                CmdCommunicationHeRun();
+              
                 if (!comAudioSource.isPlaying)
                 {
                     CmdComSound(2);
+                    CmdCommunicationHeRun();
                 }
             }
             else if (Input.GetButtonDown("Communication_StayHere"))
             {
-                CmdCommunicationStay();
+                
 
                 if (!comAudioSource.isPlaying)
                 {
                     CmdComSound(3);
+                    CmdCommunicationStay();
                 }
             }
         }
