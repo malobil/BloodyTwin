413f2cf3df20d08a796677a98e1c1f88cb58ea1b
Arthur Manoux <arthur.manoux@wanadoo.fr>
2018-05-18 13:16:06 +0200

    tuto

---
+++ Assets/Scripts/Lobby/Lobby_Arthur_Tuto/TutoManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TutoManager : MonoBehaviour 
+{
+
+	public Transform tutoB;
+	public Transform tutoS;
+	public Transform tutoI;
+	public GameObject arrowRight;
+	public GameObject arrowLeft;
+
+	private Transform currentTuto = null;
+	private int index = 0;
+
+	public void ShowTuto (Transform tutoToShow)
+	{
+		if ( currentTuto != null)
+		{
+			currentTuto.gameObject.SetActive(false);
+		}
+
+		currentTuto = tutoToShow;
+		tutoToShow.gameObject.SetActive(true);
+		Debug.Log("dd") ;
+	}
+
+	public void UnShowTuto()
+	{
+		currentTuto.gameObject.SetActive(false);
+		currentTuto = null;
+		index = 0;
+	}
+
+	void NextTuto ()
+	{
+		currentTuto.GetChild(index).gameObject.SetActive(false);
+		index++;
+		currentTuto.GetChild(index).gameObject.SetActive(true);
+		arrowLeft.SetActive(true);
+
+		if (currentTuto.GetChild(index+1)== null)
+		{
+			arrowRight.SetActive(false);
+		}
+	}
+
+	void PreviousTuto()
+	{		
+		currentTuto.GetChild(index).gameObject.SetActive(false);
+		index--;
+		currentTuto.GetChild(index).gameObject.SetActive(true);
+		arrowRight.SetActive(true);
+
+		if (currentTuto.GetChild(index-1)== null)
+		{
+			arrowRight.SetActive(false);
+		}
+	}
+}
