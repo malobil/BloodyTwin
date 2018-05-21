320dd6dcf663df27a7d2c817c3aaec8107751861
Kevin Ibanez <ibanez13keke@gmail.com>
2018-05-21 10:51:41 +0200

    *.*

--- Assets/Scripts/Characters/Intru/Script_Player_Moves.cs
+++ Assets/Scripts/Characters/Intru/Script_Player_Moves.cs
@@ -209,7 +209,10 @@ namespace UnityStandardAssets.Characters
 
         public void Die()
         {
-            if(!isLocalPlayer)
+            Destroy(this.gameObject);
+            NetworkServer.UnSpawn(this.gameObject);
+
+            if (!isLocalPlayer)
             {
                 return;
             }
@@ -218,8 +221,6 @@ namespace UnityStandardAssets.Characters
                 cameraTransform.parent = null;
                 Script_UI_InGame_Manager.Instance.IntruderLoose();
                 Script_UI_InGame_Manager.Instance.CmdIntruderDie();
-            Destroy(this.gameObject);
-                NetworkServer.UnSpawn(this.gameObject);
                 //NetworkManager.singleton.StopClient();
                 Destroy(gameObject);
         }
