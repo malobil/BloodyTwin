2845fa559ca0f0d3327cbf26d339ea83e983b40d
Kevin Ibanez <ibanez13keke@gmail.com>
2018-05-22 09:15:35 +0200

    *.*

--- Assets/Scripts/Characters/Camera/Script_Spectre_Possess_Move.cs
+++ Assets/Scripts/Characters/Camera/Script_Spectre_Possess_Move.cs
@@ -74,7 +74,7 @@ public class Script_Spectre_Possess_Move : MonoBehaviour
     {
         Transform camera = Camera.main.transform;
         rbComponent.AddForce(camera.forward * ejectionSpeed, ForceMode.Impulse);
-        //associateScriptPossesion.UnPossessObject();
+        associateScriptPossesion.UnPossessObject();
         StartCoroutine(ProjectionEnd());
         isProject = true;
         ejectionSpeed = 0;
@@ -95,7 +95,7 @@ public class Script_Spectre_Possess_Move : MonoBehaviour
 
     IEnumerator ProjectionEnd()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(3f);
         isProject = false;
         print(isProject);
     }
