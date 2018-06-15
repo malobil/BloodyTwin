faa0e960346f4f9c3e95d544e27ee84921b4cb72
MadFox <otaku.suprem@gmail.com>
2018-06-15 12:36:42 +0200

    door lock

---
+++ Assets/Scripts/Objets_Hantes/Script_PossessDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Script_PossessDoor : NetworkBehaviour {
+
+    private bool isLock;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            LockDoor();
+        }
+	}
+
+    [ClientRpc]
+    public void LockDoor ()
+    {
+        isLock = !isLock;
+    }
+}
