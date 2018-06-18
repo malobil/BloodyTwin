6b170a8ab9d9ef9ebe8d98f767e67e267fd18a36
MadFox <otaku.suprem@gmail.com>
2018-06-18 16:18:10 +0200

    Door lock
    update

--- Assets/Scripts/Other/Script_Door.cs
+++ Assets/Scripts/Other/Script_Door.cs
@@ -10,6 +10,7 @@ public class Script_Door : NetworkBehaviour {
     private Animation doorAnimation;
     [SyncVar(hook = "DoorState")]
     private bool opened = false;
+    [SyncVar(hook = "LockingDoor")]
     private bool lockedDoor = false;
 
 	// Use this for initialization
