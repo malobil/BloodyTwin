4dec0277ac7842d93d2e5829618def3280514310
Kevin Ibanez <kevin.ibanez@e-artsup.net>
2018-06-21 13:10:59 +0200

    Debug door spectre

--- Assets/Scripts/Characters/Spectre/Script_Spectre_Moves_Door.cs
+++ Assets/Scripts/Characters/Spectre/Script_Spectre_Moves_Door.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Script_Spectre_Moves_Door : MonoBehaviour {
+public class Script_Spectre_Moves_Door : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update ()
     {
 		if(Input.GetKeyDown("e"))
         {
-            GetComponent<Script_Door>().SpectreChangeState();
+            CmdDoor();
             Debug.Log("state");
         }
 	}
+
+    [Command]
+    void CmdDoor()
+    {
+        GetComponent<Script_Door>().SpectreChangeState();
+    }
 }
