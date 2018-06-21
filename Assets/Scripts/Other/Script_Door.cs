9f38d9216db0a57ad2e37ab976d5294120eba79a
Kevin Ibanez <kevin.ibanez@e-artsup.net>
2018-06-21 17:09:28 +0200

    *.*

--- Assets/Scripts/Other/Script_Door.cs
+++ Assets/Scripts/Other/Script_Door.cs
@@ -68,13 +68,6 @@ public class Script_Door : NetworkBehaviour {
         PlayDoorSound(4);
     }
 
-    [Command]
-    public void CmdLockingDoor()
-    {
-        RpcLockingDoor();
-        //Debug.Log("Lock");
-    }
-
     [ClientRpc]
     public void RpcLockingDoor ()
     {
